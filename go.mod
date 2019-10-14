a56df008efee03b533d13ab734c9c07a2cf2ff49
Junlapong L <junlapong@gmail.com>
2019-10-15 02:25:28 +0700

    add redirect http to https

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-https-server
 
 go 1.13
+
+require golang.org/x/net v0.0.0-20191011234655-491137f69257
--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 func main() {
+
+	// redirect every http request to https
+	go http.ListenAndServe(":8080", http.HandlerFunc(redirect))
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)
@@ -32,6 +37,9 @@ func main() {
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
 	}
 
+	// Enable http2
+	http2.ConfigureServer(srv, nil)
+
 	log.Println("** Service Started on Port 8443 **")
 	err := srv.ListenAndServeTLS("cert/localhost.pem", "cert/localhost-key.pem")
 
@@ -43,4 +51,18 @@ func main() {
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	io.WriteString(w, `{"status":"ok"}`+"\n")
+}
+
+func redirect(w http.ResponseWriter, req *http.Request) {
+	// remove/add not default ports from req.Host
+	//target := "https://" + req.Host + req.URL.Path
+	target := "https://localhost:8443" + req.URL.Path
+	if len(req.URL.RawQuery) > 0 {
+		target += "?" + req.URL.RawQuery
+	}
+
+	log.Printf("redirect to: %s", target)
+	http.Redirect(w, req, target,
+		// see comments below and consider the codes 308, 302, or 301
+		http.StatusPermanentRedirect)
 }
